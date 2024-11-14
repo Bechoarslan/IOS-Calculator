db41bc3e212492ad0c5158282a6aec97581c553c
Berkay Arslan <barslann09@gmail.com>
2024-11-14 10:00:35 +0300

    Calculator completed

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,132 +10,89 @@ import Foundation
 
 struct CalculatorLogic {
     private var newNumber : Double?
-    private var collectedNumber : (number: Double?,symbol : String?)?
-  
+    private var collectedNumber : (number: Double,symbol : String?)?
+    
     mutating func setNumber(_ number : Double)
     {
         newNumber = number
     }
     
     
-  mutating func calculate(with input: String) ->
+    mutating func calculate(with input: String) ->
     Double?    {
         if let n = newNumber {
             
             
             switch input {
             case "AC":
-                collectedNumber?.number = nil
-                collectedNumber?.symbol = nil
+                collectedNumber =  nil
                 return 0
             case "+/-":
                 return n * -1
             case "%":
                 return n * 0.01
             case "=":
-                return calculate(n)
+                return doCalculation(n)
+               
             default:
-                if collectedNumber == nil {
-                    collectedNumber = (number: n,symbol: input)
-                }
-                else if collectedNumber?.number == nil && collectedNumber?.symbol == nil {
-                    collectedNumber?.number = n
-                    collectedNumber?.symbol = input
-                }
-                else if collectedNumber?.number == nil {
-                    collectedNumber = (number: n,symbol: input)
-                    print("Number is nill")
-                }
-                else if collectedNumber?.symbol == nil
+                if collectedNumber != nil && collectedNumber?.symbol == nil
                 {
                     print("Symbol is nill")
-                    print(collectedNumber?.symbol)
-                    return startCalculation(n,input)
+                    collectedNumber = (collectedNumber!.number, input)
+                   return doCalculation(n)
                 }
-                else
-                {
-                    print("None of them nill")
-                    return startCalculation(n,collectedNumber?.symbol)
+                else if  collectedNumber?.number != nil && collectedNumber?.symbol != nil{
+                    print("Nothing is nil")
+                    return doCalculation(n)
                 }
+                else {
+                    print("Collected number is nil")
+                    collectedNumber = (n, input)
+                }
+                
+                
+                
+                
                 
             }
             
-           
+            return nil
             
         }
         
+        
         return nil
+        
+        
         
     }
     
-    mutating func calculate(_ number2 : Double) -> Double?
+
+   mutating func doCalculation(_ number: Double) -> Double?
     {
-        if let number = collectedNumber?.number, let symbol = collectedNumber?.symbol {
+        if let firstNumber = collectedNumber?.number, let symbol = collectedNumber?.symbol {
             switch symbol {
             case "+":
-                let newValue = number + number2
                 collectedNumber?.symbol = nil
-                collectedNumber?.number = nil
-                return newValue
-                case "-":
-                let newValue = number - number2
-                collectedNumber?.symbol = nil
-                collectedNumber?.number = nil
-                return newValue
-            case "×":
-                let newValue = number * number2
-                collectedNumber?.symbol = nil
-                collectedNumber?.number = nil
-                return newValue
-                case "÷":
-                let newValue = number / number2
-                collectedNumber?.symbol = nil
-                collectedNumber?.number = nil
-                return newValue
-            default:
-                print("There is no input like that")
-            }
-        }
-        return nil
-        
-    }
-    mutating func startCalculation(_ number2 : Double, _ input : String? = nil) -> Double?
-    {
-        
-        if let number = collectedNumber?.number {
-            switch input {
-            case "+":
-                var newValue = collectedNumber!.number! + number2
-                collectedNumber?.number = newValue
-                collectedNumber?.symbol = nil
-      
-                
-                return newValue
+                collectedNumber?.number = firstNumber + number
+                return firstNumber + number
             case "-":
-                var newValue = collectedNumber!.number! - number2
-                collectedNumber?.number = newValue
+                print("-")
                 collectedNumber?.symbol = nil
-            case "×":
-                var newValue = collectedNumber!.number! * number2
-                collectedNumber?.number = newValue
+                collectedNumber?.number = firstNumber - number
+                return firstNumber - number
+                case "×":
                 collectedNumber?.symbol = nil
-                return newValue
+                collectedNumber?.number = firstNumber * number
+                return firstNumber * number
                 case "÷":
-                var newValue = collectedNumber!.number! / number2
-                collectedNumber?.number = newValue
                 collectedNumber?.symbol = nil
-                return newValue
+                collectedNumber?.number = firstNumber / number
+                return firstNumber / number
             default:
-                print("There is not a valid input")
+                print("There is no valid input")
             }
-            
         }
-        
         return nil
     }
-    
-    
- 
-    
-    
 }
